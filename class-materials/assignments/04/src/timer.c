eb7e156cacbd07cb1325f9f4340200a7c14f4f22
Maximillian Sonderegger <maxmsonderegger@gmail.com>
2024-01-20 13:56:44 -0500

    Rename timer_init to be consistent with other similar functions

--- class-materials/assignments/04/src/timer.c
+++ class-materials/assignments/04/src/timer.c
@@ -1,22 +1,17 @@
 #include "timer.h"
 #include "machinevec.h"
 #include "parameters.h"
+#include "riscv.h"
 
 // arrange to receive timer interrupts.
 // they will arrive in machine mode at
 // machinevec
-void timer_init() {
+void timer_inithart() {
   // each CPU has a separate source of timer interrupts.
   int id = riscv_r_mhartid();
 
   // ask the CLINT for a timer interrupt.
   *(uint64 *)MEMLAYOUT_CLINT_MTIMECMP(id) = *(uint64 *)MEMLAYOUT_CLINT_MTIME + PARAMETERS_INTERRUPT_INTERVAL;
-
-  // set the machine-mode trap handler.
-  riscv_w_mtvec((uint64)machinevec);
-
-  // enable machine-mode interrupts.
-  riscv_w_mstatus(riscv_r_mstatus() | RISCV_MSTATUS_MIE);
 
   // enable machine-mode timer interrupts.
   riscv_w_mie(riscv_r_mie() | RISCV_MIE_MTIE);
