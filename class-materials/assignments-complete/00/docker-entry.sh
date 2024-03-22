a57be1f3874b35508825b65805702d231e1f1c9b
Maximillian Sonderegger <maxmsonderegger@gmail.com>
2024-03-22 18:23:56 -0400

    Templated assignment 00

--- class-materials/assignments-complete/00/docker-entry.sh
+++ class-materials/assignments-complete/00/docker-entry.sh
@@ -1,5 +1,7 @@
+# BEGIN DELETE BLOCK
 session="umass-os"
 
 tmux new-session -d -s $session -x - -y - "make qemu-gdb"
 tmux split-window -hb -t $session:0 "gdb-multiarch"
 tmux attach-session -t $session
+# END DELETE BLOCK
