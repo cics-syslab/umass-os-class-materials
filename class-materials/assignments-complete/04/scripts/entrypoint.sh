2f687a2d253e610eb350e546eaa982e87b14a6d0
Maximillian Sonderegger <maxmsonderegger@gmail.com>
2024-04-18 12:59:52 -0400

    New entry points for docker files

--- class-materials/assignments-complete/04/scripts/entrypoint.sh
+++ class-materials/assignments-complete/04/scripts/entrypoint.sh
@@ -1,5 +1,4 @@
 session="umass-os"
 
-tmux new-session -d -s $session -x - -y - "make qemu-gdb"
-tmux split-window -hb -t $session:0 "gdb-multiarch"
+tmux new-session -d -s $session -x - -y - "make && tmux split-window -hb -t $session:0 \"gdb-multiarch\" && make qemu-gdb"
 tmux attach-session -t $session
